1842cfbb7d9114725fa52acb2fd48b790378c0b6
georginawalsh <20093147>
2023-09-15 09:07:01 +0100

    Demo optional properties and parameters.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -58,16 +58,22 @@ console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
 function sortColleagues(
     colleagues: Colleague[],
-    sorter: (c1: Colleague, c2: Colleague) => number
+    sorter: (c1: Colleague, c2: Colleague) => number,
+    max? : number
   ): EmailContact[] {
-    const sorted = colleagues.sort(sorter); // Colleague[] inferred
-    const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
-    return result 
+    let end = colleagues.length;
+    if (max !== undefined) {
+       end = max < 2 ? 1 : max
+    }
+    const sorted = colleagues.sort(sorter);
+    const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+    return fullResult.slice(0,end)
   }
+  // Test invocations
+  console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
+  console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+  console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
   
-  console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
-  console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
-
 
 function findFriends(fs: Friend[], callback: (friend: any) => any) {
     var found = new Array()
@@ -76,7 +82,7 @@ function findFriends(fs: Friend[], callback: (friend: any) => any) {
             found.push(f.name);
         }
     });
-    return found;
+    return found; 
 }
 
   console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
